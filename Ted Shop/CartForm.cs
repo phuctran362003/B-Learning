654aea2fd94d1d37fdd1af026fd1651fa7187555
phuctran362003 <trangiaphuc362003181@gmail.com>
2024-03-16 01:20:35 +0700

    Add window form layout

--- Ted Shop/CartForm.cs
+++ Ted Shop/CartForm.cs
@@ -1,8 +1,8 @@
 namespace Ted_Shop
 {
-    public partial class Form1 : Form
+    public partial class CartForm : Form
     {
-        public Form1()
+        public CartForm()
         {
             InitializeComponent();
         }
---
+++ Ted Shop/DetailProductForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ted_Shop
+{
+    public partial class DetailProductForm : Form
+    {
+        public DetailProductForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
